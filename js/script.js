939ab79c5b183a1b497c3e815ef87734dd824cca
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-12-19 19:40:20 +0000

    added function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,43 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Dec 2022
 // This file contains the JS functions for index.html
 
-"use strict"
-
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit6-03-JS/sw.js", {
+    scope: "/ICS2O-Unit6-03-JS/",
   })
 }
 
+
 /**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+ * Get API info.
+*/
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
+
+const getImage = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("api-image").innerHTML =
+    '<img src="' + 
+      jsonData.weather_icon + 
+      '" alt="API image" class="center" ' +
+      '>'
+    if (jsonData.weather != "none") {
+      document.getElementById("temperature").innerHTML =
+      "<p>temperature: " + '<a href="' + jsonData.weather_description + '">' + jsonData.weather_description  + "</a>"
+  } else {
+    document.getElementById("temperature").innerHTML = "<p>Temperature: unknown</p>"
+  }
+  } catch (err) {
+    console.log(err)
+  }
 }
+
+getImage("https://api.openweathermap.org")
